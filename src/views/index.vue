07b183f163dcc16fbd5eccff6dedcf4288310dbf
lify <lify@crunii.com>
2018-11-06 10:50:57 +0800

    update

--- src/views/index.vue
+++ src/views/index.vue
@@ -9,17 +9,6 @@
             <a type="ghost" href="/page2">Start iView</a>
         </h2>
 
-        <div class="container">
-            <header>
-                <slot name="header"></slot>
-            </header>
-            <main>
-                <slot></slot>
-            </main>
-            <footer>
-                <slot name="footer"></slot>
-            </footer>
-        </div>
 
         <!--全局組件-->
         <my-component>
@@ -31,25 +20,45 @@
         </my-component>
 
         <!--局部組件-->
-        <my-component1></my-component1>
+        <my-component1 message="props message"></my-component1>
+
+
     </div>
 
+</template>
+<template id="myComponent2">
+    <div>
+        <h2>{{ msg }}</h2>
+
+        <button @click="showMsg">Show Message</button>
+    </div>
 </template>
 <script>
 
     export default {
+
+        data: function () {
+            return {
+                msg: 'This is a component!'
+            }
+        },
         methods: {
             handleStart () {
                 this.$Modal.info({
                     title: 'Bravo',
                     content: 'Now, enjoy the convenience of iView.'
                 });
+            },
+            showMsg: function () {
+                alert(this.msg)
             }
         },
         components: {
             'my-component1': {
-                template: '<div>A custom component11!' +
-                '<slot name="mySlot"></slot></div>'
+                props: ['message'],
+                template: '<div>A custom component11! {{message}}' +
+                '<slot name="mySlot"></slot>' +
+                '</div>'
             }
         }
     }
